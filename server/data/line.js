5e1cb49b4e6c9431ecb7321901e16c90bf166470
terrence1028 <143954593+terrence1028@users.noreply.github.com>
2024-05-23 14:15:46 +0800

    line.js

--- server/data/line.js
+++ server/data/line.js
@@ -1,13 +1,13 @@
 module.exports = {
     lines: [
         {
-            name: '隧道增加数量',
+            name: '隧道增加數量',
             type: 'line',
             stack: 'Total',
             data: [220, 230, 101, 134, 90, 230, 210]
         },
         {
-            name: '地质预测数量',
+            name: '地質預測數量',
             type: 'line',
             stack: 'Total',
             data: [220, 182, 191, 234, 290, 330, 310]
